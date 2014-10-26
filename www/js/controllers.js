d953217a1b05b191142e3b85e3bb5af91d423a3d
bradyz <brady.zhou@utexas.edu>
2014-10-26 00:41:28 -0500

    BZ added city binding

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,19 @@
 angular.module('starter.controllers', [])
 
+.controller('WelCtrl', function($scope, $rootScope) {
+  $scope.setCity = function (place) {
+    $rootScope.city = place;
+    console.log(place);
+  };
+})
 
 // A simple controller that fetches a list of data from a service
-.controller('HostIndexCtrl', function($scope, HostService) {
-  // "Hosts" is a service returning mock data (services.js)
+.controller('HostIndexCtrl', function($scope, $rootScope, HostService) {
+  $scope.city = $rootScope.city;
   $scope.hosts = HostService.all();
 })
 
-
 // A simple controller that shows a tapped item's data
 .controller('HostDetailCtrl', function($scope, $stateParams, HostService) {
-  // "Hosts" is a service returning mock data (services.js)
   $scope.host = HostService.get($stateParams.hostId);
 });
