cee3cce7171c545bce0aab16d4ed884779758f56
Haley Owen <haley@Haleys-MacBook-Pro.local>
2014-10-26 12:27:59 -0500

    added messaging

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('starter.controllers', [])
 .controller('WelCtrl', function($scope, $rootScope) {
   $scope.setCity = function (place) {
     $rootScope.city = place;
-    console.log(place);
+    //console.log(place);
   };
 })
 
@@ -16,4 +16,38 @@ angular.module('starter.controllers', [])
 // A simple controller that shows a tapped item's data
 .controller('HostDetailCtrl', function($scope, $stateParams, HostService) {
   $scope.host = HostService.get($stateParams.hostId);
-});
+})
+
+.controller('Messages', function($scope, $timeout, $ionicScrollDelegate){
+
+    var alternate,
+        isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+
+    $scope.sendMessage = function(){
+        alternate = !alternate;
+        $scope.messages.push({
+            userId: alternate ? '12345' : '54321',
+            photo: alternate ? 'http://graph.facebook.com/1218369214/picture?width=120' : 'http://graph.facebook.com/556575560/picture?width=120',
+            text: $scope.data.message
+        });
+        delete $scope.data.message;
+        $ionicScrollDelegate.scrollBottom(true);
+    }
+
+    $scope.inputUp = function(){
+        if(isIOS) $scope.data.keyboardHeight = 216;
+        $timeout(function(){
+            $ionicScrollDelegate.scrollBottom(true);
+        }, 300);
+
+    }
+    $scope.inputDown = function(){
+        if(isIOS) $scope.data.keyboardHeight = 0;
+        $ionicScrollDelegate.resize();
+    }
+
+    $scope.data = {};
+    $scope.myId = '12345';
+    $scope.messages = ["asdf"];
+ });
+
--- www/js/directives.js
+++ www/js/directives.js
@@ -43,4 +43,42 @@ angular.module('starter')
             }
         }
     };
+})
+
+.directive('input', function($timeout){
+    return {
+        restrict: 'E',
+        scope: {
+            'returnClose': '=',
+            'onReturn': '&',
+            'onFocus': '&',
+            'onBlur': '&'
+        },
+        link: function(scope, element, attr){
+            element.bind('focus', function(e){
+                if(scope.onFocus){
+                    $timeout(function(){
+                        scope.onFocus();
+                    });
+                }        
+            });
+            element.bind('blur', function(e){
+                if(scope.onBlur){
+                    $timeout(function(){
+                        scope.onBlur();
+                    });
+                }
+            });
+            element.bind('keydown', function(e){
+                if(e.which == 13){
+                    if(scope.returnClose) element[0].blur();
+                    if(scope.onReturn){
+                        $timeout(function(){
+                            scope.onReturn();
+                        });                        
+                    }
+                } 
+            });   
+        }
+    };
 });
